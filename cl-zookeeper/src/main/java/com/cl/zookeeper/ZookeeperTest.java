323b4ddb27ab3755441415cc50b73042db6af00a
clbhii <673445075@qq.com>
2016-06-29 10:34:44 +0800

    12
    21

--- cl-zookeeper/src/main/java/com/cl/zookeeper/ZookeeperTest.java
+++ cl-zookeeper/src/main/java/com/cl/zookeeper/ZookeeperTest.java
@@ -5,9 +5,34 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Test;
+
 
 public class ZookeeperTest {
+	@Test
+	public void test1() throws Exception{
+		// 创建一个与服务器的连接
+		ZooKeeper zk = new ZooKeeper("localhost:2181", 1000, new Watcher() {
+			// 监控所有被触发的事件
+			public void process(WatchedEvent event) {
+				System.out.println("已经触发了" + event.getType() + "事件！" +event.getState());
+			}
+		});
+		Stat exists = zk.exists("/testRootPath", true);
+		if(exists == null)
+		zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		
+		zk.delete("/testRootPath",-1);
+		Thread.sleep(100000);
+		zk.close();
+	}
+	
+	
 
+
+	
+	
 	public static void main(String[] args) throws Exception {
 		// 创建一个与服务器的连接
 		ZooKeeper zk = new ZooKeeper("localhost:2181", 1000, new Watcher() {
