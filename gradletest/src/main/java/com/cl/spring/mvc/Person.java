2a726f2d60fa0dec991bbaeb18dc82ae658947f4
clbhii <clbhii@163.com>
2020-03-15 10:03:13 +0800

    gradle test

---
+++ gradletest/src/main/java/com/cl/spring/mvc/Person.java
@@ -0,0 +1,31 @@
+package com.cl.spring.mvc;
+
+public class Person {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	private String name;
+	private int age;
+	private User user;
+}
